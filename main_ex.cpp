4a7fd28fa3235b259f38c50e6c8eca9c6eafb5f8
vitamin-abcd <1006047327@qq.com>
2018-12-18 19:30:04 +0800

    多线程版本

--- main_ex.cpp
+++ main_ex.cpp
@@ -171,9 +171,9 @@ int main(int argc, char *argv[]) {
     pthread_t callThd[NET_SIZE];
     for (int i = 0; i < NET_SIZE; i++) {
         int rc = pthread_create(&callThd[i], NULL, run, (void *) &inferRequest[NET_SIZE - i - 1]);
+        pthread_join(callThd[i], NULL);
     }
     for (int i = 0; i < NET_SIZE; i++) {
-        pthread_join(callThd[i], NULL);
     }
 
 
