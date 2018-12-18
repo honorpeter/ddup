8dabe634b33f0866b92832ce7149dbe61ed0c978
vitamin-abcd <1006047327@qq.com>
2018-12-18 19:24:20 +0800

    多线程版本

--- main_ex.cpp
+++ main_ex.cpp
@@ -30,7 +30,7 @@ void createPlugin(InferencePlugin &plugin, int index) {
         plugin.AddExtension(std::make_shared<Extensions::Cpu::CpuExtensions>());
     }
     plugin.SetConfig({{PluginConfigParams::KEY_CPU_BIND_THREAD, PluginConfigParams::YES}});
-    plugin.SetConfig({{PluginConfigParams::KEY_DEVICE_ID, "1"}});
+    //plugin.SetConfig({{PluginConfigParams::KEY_DEVICE_ID, "1"}});
     printPluginVersion(plugin, std::cout);
 }
 
@@ -162,9 +162,9 @@ int main(int argc, char *argv[]) {
     createPlugin(plugin[0],0);
     readNet(reader[0]);
 
-    executableNetwork[0] = plugin[0].LoadNetwork(reader[0].getNetwork(), {});
     for (int i = 0; i < NET_SIZE; i++) {
-        inferRequest[i] = executableNetwork[0].CreateInferRequest();
+        executableNetwork[i] = plugin[0].LoadNetwork(reader[0].getNetwork(), {});
+        inferRequest[i] = executableNetwork[i].CreateInferRequest();
         fillData(inferRequest[i], reader[0]);
     }
 
