7cf7ac43fc9fe2288c1532a4971d923fbf383ab6
adai <adai@meili-inc.com>
2018-12-18 09:23:28 +0000

    cross

---
+++ crossroad_camera_demo/CMakeFiles/crossroad_camera_demo.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/opt/intel/computer_vision_sdk_2018.4.420/inference_engine/samples/crossroad_camera_demo/main.cpp" "/opt/intel/computer_vision_sdk_2018.4.420/inference_engine/samples/crossroad_camera_demo/CMakeFiles/crossroad_camera_demo.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GFLAGS_IS_A_DLL=0"
+  "HAVE_AVX2"
+  "HAVE_SSE"
+  "USE_OPENCV"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "common/format_reader"
+  "/opt/intel/computer_vision_sdk_2018.4.420/inference_engine/include"
+  "intel64/Release/lib/gflags/include"
+  "common"
+  "/opt/intel/computer_vision_sdk_2018.4.420/opencv/include"
+  "/opt/intel/computer_vision_sdk_2018.4.420/inference_engine/src/extension"
+  "thirdparty/gflags/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/opt/intel/computer_vision_sdk_2018.4.420/inference_engine/samples/ie_cpu_extension/CMakeFiles/ie_cpu_extension.dir/DependInfo.cmake"
+  "/opt/intel/computer_vision_sdk_2018.4.420/inference_engine/samples/thirdparty/gflags/CMakeFiles/gflags_nothreads_static.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
