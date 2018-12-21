5ca2bf85f5822164a36440ee9480fbf407846da9
vitamin-abcd <1006047327@qq.com>
2018-12-21 20:34:07 +0800

    多线程版本

--- main_ex.cpp
+++ main_ex.cpp
@@ -218,12 +218,15 @@ void ex_pic(float *phead, int size) {
     }
 
     /** 均值化 再减去 均值 **/
-    for (int y = 0;  y < rgb.rows; ++y) {
+    for (int y = 0; y < rgb.rows; ++y) {
         for (int x = 0; x < rgb.cols; ++x) {
             if (y < 10 && x < 10) {
-                printf("%d_%d : %hhu - %f \n", y, x, rgb.at<cv::Vec3b>(y, x)[0], mean_arr[y * width + x]);
-                printf("%d_%d : %hhu - %f \n", y, x, rgb.at<cv::Vec3b>(y, x)[1], mean_arr[y * width + x + delta_green]);
-                printf("%d_%d : %hhu - %f \n", y, x, rgb.at<cv::Vec3b>(y, x)[2], mean_arr[y * width + x + delta_blue]);
+                printf("%d_%d %d: %hhu - %f \n", y, x, y * width + x, rgb.at<cv::Vec3b>(y, x)[0],
+                       mean_arr[y * width + x]);
+                printf("%d_%d %d: %hhu - %f \n", y, x, y * width + x + delta_green, rgb.at<cv::Vec3b>(y, x)[1],
+                       mean_arr[y * width + x + delta_green]);
+                printf("%d_%d %d: %hhu - %f \n", y, x, y * width + x + delta_blue, rgb.at<cv::Vec3b>(y, x)[2],
+                       mean_arr[y * width + x + delta_blue]);
             }
             rgb.at<cv::Vec3f>(y, x)[0] = (rgb.at<cv::Vec3b>(y, x)[0] - mean_arr[y * width + x]) / 255.0f;
             rgb.at<cv::Vec3f>(y, x)[1] = (rgb.at<cv::Vec3b>(y, x)[1] - mean_arr[y * width + x + delta_green]) / 255.0f;
