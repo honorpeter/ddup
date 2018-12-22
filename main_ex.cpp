5ed8de8941751174ed231d298d660f1897269998
vitamin-abcd <1006047327@qq.com>
2018-12-22 15:29:32 +0800

    多线程版本

--- main_ex.cpp
+++ main_ex.cpp
@@ -261,11 +261,11 @@ void ex_pic(float *phead, int size) {
     }
     resized.release();
 
-    crop(d_mean, phead, 0, 0, 224, 224, 1);
-//    crop(d_mean, phead, 0, 11, 224, 224, 0);
-//    crop(d_mean, phead, 32, 21, 224, 224, 0);
-//    crop(d_mean, phead, 32, 32, 224, 224, 0);
-//    flip(tmp, phead, 224, 224, 0);
+    crop(d_mean, phead, 0, 0, 224, 224, 0);
+    crop(d_mean, phead, 0, 11, 224, 224, 0);
+    crop(d_mean, phead, 32, 21, 224, 224, 0);
+    crop(d_mean, phead, 32, 32, 224, 224, 0);
+    flip(tmp, phead, 224, 224, 1);
     print_head_from_arr(phead, 10);
 
 
