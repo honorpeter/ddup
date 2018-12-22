69e18bc9a025d85e4ad1f10a547f71121818e1f8
vitamin-abcd <1006047327@qq.com>
2018-12-22 16:35:30 +0800

    多线程版本

--- main_ex.cpp
+++ main_ex.cpp
@@ -310,10 +310,12 @@ void fillData(InferRequest &inferRequest, CNNNetReader &reader) {
         float pInput2[224 * 224 * 3];
         size_t read = fread((void *) pInput2, sizeof(float), (size_t) 224 * 224 * 3, pInputFile);
         read = fread((void *) pInput2, sizeof(float), (size_t) 224 * 224 * 3, pInputFile);
+        read = fread((void *) pInput2, sizeof(float), (size_t) 224 * 224 * 3, pInputFile);
+
         ex_pic(pInput);
 
         float sum = 0;
-        int offset = 224 * 224 * 3 * 1;
+        int offset = 224 * 224 * 3 * 2;
         print_head_from_arr(pInput2, 21);
         print_head_from_arr(pInput, 21,offset);
         for (int j = 0; j < 224 * 224 * 3; ++j) {
