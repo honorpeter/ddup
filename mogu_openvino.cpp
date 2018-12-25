31477532df6c5a432c2588ecf821b1e13cc7ce3f
vitamin-abcd <1006047327@qq.com>
2018-12-25 11:26:23 +0800

    多线程版本

--- mogu_openvino.cpp
+++ mogu_openvino.cpp
@@ -323,9 +323,10 @@ void Openvino_Net::collectOutPut(InferRequest &inferRequest, Config &config, Out
         const LockedMemory<const void> memLocker = outputBlob->cbuffer();
         // todo 将来可能需要使用泛型来指定精度
         const float *outputBuffer = memLocker.as<PrecisionTrait<Precision::FP32>::value_type *>();
-        output.data = outputBuffer;
         printf("End to collect output\n");
         fflush(stdout);
+        output.data = outputBuffer;
+
         SizeVector shapesVector = outputBlob->getTensorDesc().getDims();
         int i = 0;
         for (auto shapeIteator = shapesVector.begin(); shapeIteator != shapesVector.end(); ++shapeIteator, ++i){
