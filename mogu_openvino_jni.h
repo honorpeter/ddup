d60d0cdfd30435839015eba7ea77b65d684fa18f
vitamin-abcd <1006047327@qq.com>
2018-12-25 21:26:44 +0800

    jni demo

--- mogu_openvino_jni.h
+++ mogu_openvino_jni.h
@@ -5,10 +5,8 @@
 #ifndef DDUP_MOGU_OPENVINO_JNI_H
 #define DDUP_MOGU_OPENVINO_JNI_H
 
-#include <jni.h>
+#include "jni.h"
 #include "com_mogujie_algo_openvino_jni_MoguOpenvino.h"
-#include "com_mogujie_algo_openvino_jni_MoguOpenvino_InputBlob.h"
 #include "com_mogujie_algo_openvino_jni_MoguOpenvino_OpenvinoCfg.h"
-#include "com_mogujie_algo_openvino_jni_MoguOpenvino_OutputBlob.h"
 #include "mogu_openvino.h"
 #endif //DDUP_MOGU_OPENVINO_JNI_H
