c902343b1d5a3adfb03d08a25381b19d1908889d
adai <adai@meili-inc.com>
2018-12-25 20:38:01 +0800

    jni.h

--- com_mogujie_algo_openvino_jni_MoguOpenvino.h
+++ com_mogujie_algo_openvino_jni_MoguOpenvino.h
@@ -18,10 +18,10 @@ JNIEXPORT jint JNICALL Java_com_mogujie_algo_openvino_jni_MoguOpenvino_create
 /*
  * Class:     com_mogujie_algo_openvino_jni_MoguOpenvino
  * Method:    inference
- * Signature: ([CIII)Lcom/mogujie/algo/openvino/jni/MoguOpenvino/OutputBlob;
+ * Signature: (Ljava/lang/String;[CIII)Lcom/mogujie/algo/openvino/jni/MoguOpenvino/OutputBlob;
  */
 JNIEXPORT jobject JNICALL Java_com_mogujie_algo_openvino_jni_MoguOpenvino_inference
-  (JNIEnv *, jclass, jcharArray, jint, jint, jint);
+  (JNIEnv *, jclass, jstring, jcharArray, jint, jint, jint);
 
 /*
  * Class:     com_mogujie_algo_openvino_jni_MoguOpenvino
